7a14d75b128d3ee6cca80e593be0cc9339f8cde7
MaoMaoPrince <31109364+MaoMaoPrince@users.noreply.github.com>
2023-06-09 22:30:15 +0100

    inital

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,128 +1,47 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { useState } from 'react';
+import { useReward } from 'react-rewards';
+import React from 'react';
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
+const IndexPage = () => {
+  return <Cannon />;
+};
 
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-  },
-  {
-    text: "Examples",
-    url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
-    description:
-      "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-  },
-]
+const Cannon = () => {
+  const { reward: confettiReward, isAnimating } = useReward('rewardId', 'confetti', { lifetime: 200, angle: 45, decay: 0.94, spread: 45, startVelocity: 35, elementCount: 250, elementSize: 8, zIndex: 0, position: "fixed", colors: ['#A45BF1', '#25C6F6', '#72F753', '#F76C88', '#F5F770'] });
+  const { reward: balloonsReward, isBalloonsAnimating } = useReward('balloonsReward', 'balloons', { angle: 90, spread: 90, elementCount: 50 });
+  const [isTextVisible, setIsTextVisible] = useState(false);
 
-const samplePageLinks = [
-  {
-    text: "Page 2",
-    url: "page-2",
-    badge: false,
-    description:
-      "A simple example of linking to another page within a Gatsby site",
-  },
-  { text: "TypeScript", url: "using-typescript" },
-  { text: "Server Side Rendering", url: "using-ssr" },
-  { text: "Deferred Static Generation", url: "using-dsg" },
-]
+  const handleRewardClick = () => {
+    confettiReward()
+    balloonsReward()
+    setIsTextVisible(true);
+  };
 
-const moreLinks = [
-  { text: "Join us on Discord", url: "https://gatsby.dev/discord" },
-  {
-    text: "Documentation",
-    url: "https://gatsbyjs.com/docs/",
-  },
-  {
-    text: "Starters",
-    url: "https://gatsbyjs.com/starters/",
-  },
-  {
-    text: "Showcase",
-    url: "https://gatsbyjs.com/showcase/",
-  },
-  {
-    text: "Contributing",
-    url: "https://www.gatsbyjs.com/contributing/",
-  },
-  { text: "Issues", url: "https://github.com/gatsbyjs/gatsby/issues" },
-]
+  return (
 
-const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
+    <div style={{ display: 'flex', flexDirection: "column", justifyContent: 'flex-end', alignItems: 'center', height: '100vh', width: '100vw' }}>
+      {isTextVisible && (
+        <div className="fade-in-text">
+          <p style={{ fontSize: "3.2em", textAlign: 'center', fontWeight: "bold", padding: "8px" }}>Happy Birthday <span style={{ color: "#e20b6d" }}>Anne!</span></p>
+        </div>
+      )}
+      <div style={{ height: "50vw" }} />
+      <span id="balloonsReward" />
 
-const IndexPage = () => (
-  <Layout>
-    <div className={styles.textCenter}>
-      <StaticImage
-        src="../images/example.png"
-        loading="eager"
-        width={64}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-      <h1>
-        Welcome to <b>Gatsby!</b>
-      </h1>
-      <p className={styles.intro}>
-        <b>Example pages:</b>{" "}
-        {samplePageLinks.map((link, i) => (
-          <React.Fragment key={link.url}>
-            <Link to={link.url}>{link.text}</Link>
-            {i !== samplePageLinks.length - 1 && <> · </>}
-          </React.Fragment>
-        ))}
-        <br />
-        Edit <code>src/pages/index.js</code> to update this page.
-      </p>
+      <span
+        id="rewardId"
+        style={{
+          fontSize: '4rem',
+          cursor: 'pointer',
+          transform: 'rotate(-45deg)',
+          marginBottom: '20px'
+        }}
+        onClick={handleRewardClick}
+      >
+        🎉
+      </span>
     </div>
-    <ul className={styles.list}>
-      {links.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-          >
-            {link.text} ↗
-          </a>
-          <p className={styles.listItemDescription}>{link.description}</p>
-        </li>
-      ))}
-    </ul>
-    {moreLinks.map((link, i) => (
-      <React.Fragment key={link.url}>
-        <a href={`${link.url}${utmParameters}`}>{link.text}</a>
-        {i !== moreLinks.length - 1 && <> · </>}
-      </React.Fragment>
-    ))}
-  </Layout>
-)
+  );
+};
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
-export const Head = () => <Seo title="Home" />
-
-export default IndexPage
+export default IndexPage;
